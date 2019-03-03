af7ac9b8226e58316329a2e48511590cda26bc1e
Максим Найдович <max.naid@mail.ru>
2019-03-03 10:34:12 +0500

    LogPacher begining

---
+++ Kontur.LogPacker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kontur.LogPacker
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Kontur!");
+        }
+    }
+}
