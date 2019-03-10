6d143e68a396703c12fd486d2c08a77e67159f54
Максим Найдович <max.naid@mail.ru>
2019-03-10 08:23:31 +0500

    Подготовил к реализации алгоритми

--- Kontur.LogPacker/EntryPoint.cs
+++ Kontur.LogPacker/EntryPoint.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Kontur.LogPacker
 {
@@ -6,7 +7,50 @@ namespace Kontur.LogPacker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Kontur!");
+            if (args.Length == 2)
+            {
+                var (inputFile, outputFile) = (args[0], args[1]);
+
+                if (File.Exists(inputFile))
+                {
+                    Compress(inputFile, outputFile);
+                    return;
+                }
+            }
+
+            if (args.Length == 3 && args[0] == "-d")
+            {
+                    var (inputFile, outputFile) = (args[1], args[2]);
+                    if(File.Exists(inputFile))
+                {
+                    Decompress(inputFile, outputFile);
+                    return;
+                }
+            }
+
+            ShowUsage();            
+        }
+
+        private static void ShowUsage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine($"{AppDomain.CurrentDomain.FriendlyName} [-d] <inputFile> <outputFile>");
+            Console.WriteLine("\t-d flag turns on the decompression mode");
+            Console.WriteLine();
+        }
+
+        private static void Compress(string inputFile, string outputFile)
+        {
+            using (var inputStream = File.OpenRead(inputFile))
+            using (var outputStream = File.OpenWrite(outputFile))
+                new LogPacker().Compress(inputStream, outputStream);
+        }
+
+        private static void Decompress(string inputFile, string outputFile)
+        {
+            using (var inputStream = File.OpenRead(inputFile))
+            using (var outputStream = File.OpenWrite(outputFile))
+                new LogPacker().Decompress(inputStream, outputStream);
         }
     }
 }
---
+++ Kontur.LogPacker/LogPacker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace Kontur.LogPacker
+{
+    internal class LogPacker
+    {
+        public void Compress(Stream inputStream, Stream outputStream)
+        {
+            using (var gzipStream = new GZipStream(outputStream, CompressionLevel.Optimal, true))
+                inputStream.CopyTo(gzipStream);
+        }
+
+        public void Decompress(Stream inputStream, Stream outputStream)
+        {
+            using (var gzipStream = new GZipStream(inputStream, CompressionMode.Decompress, true))
+                gzipStream.CopyTo(outputStream);
+        }
+    }
+}
